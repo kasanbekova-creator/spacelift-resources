70882db15871d48f6f9cb52cc4075fc1260775cd
Karina Asanbekova <kasanbekova@natera.com>
2026-08-27 10:35:34 -0700

    deleted

--- spacelift/workerpool.tf
+++ spacelift/workerpool.tf
@@ -1,7 +1,7 @@
 # TODO(spacelift): Kubernetes worker pool via Helm spacelift-workerpool-controller + WorkerPool CRD,
 # running as pods inside the private dvtl815-poc cluster.
 #
-# Steps (confirm with Spacelift reps):
+# Steps:
 #   1. Add the Spacelift Helm repo and install spacelift-workerpool-controller into a dedicated namespace.
 #   2. Create a WorkerPool CRD resource referencing the token/credentials from the Spacelift console.
 #   3. Wire the worker pool to the stacks in dvtl-815-infra/ and dvtl-815-resources/.
