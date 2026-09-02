ae2731c6cf78f2ee0889d775a6f75fd3efc2a855
Karina Asanbekova <kasanbekova@natera.com>
2026-09-02 11:15:28 -0700

    Add diagnostic hooks + gitignore; revert get-token role pin

--- providers.tf
+++ providers.tf
@@ -6,19 +6,26 @@ data "aws_eks_cluster" "this" {
   name = var.eks_cluster_name
 }
 
-# TODO(spacelift): set kubernetes/helm provider auth once worker auth is decided
-# (OIDC/AssumeRoleWithWebIdentity as primary, or IRSA/Pod Identity via Kyverno as fallback)
-# Replace the token value below with the appropriate exec{} or token config for the Spacelift worker.
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.this.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-  token                  = "PLACEHOLDER" # TODO(spacelift): replace with worker auth token
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", var.eks_cluster_name, "--region", "us-west-2"]
+  }
 }
 
 provider "helm" {
   kubernetes = {
     host                   = data.aws_eks_cluster.this.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-    token                  = "PLACEHOLDER" # TODO(spacelift): replace with worker auth token
+
+    exec = {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", var.eks_cluster_name, "--region", "us-west-2"]
+    }
   }
 }
